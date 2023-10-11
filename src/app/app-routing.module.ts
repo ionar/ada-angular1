43d8cf929811012164c1401d41a1a9d8b010f5c7
Ionar Rafael Michielin <irm@ionar.com.br>
2023-10-11 20:27:32 -0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomersComponent } from './pages/customers/customers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "customers", component: CustomersComponent},
+  { path: "", component: CustomersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
